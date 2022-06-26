1c74ba85b6a063c58dc23faccc03eced1ea7caa8
성정훈 <corqkrtk1@gmail.com>
2022-06-26 21:47:39 +0900

    미완성 경주게임

--- src/main/java/NumberChange.java
+++ src/main/java/NumberChange.java
@@ -1,4 +1,4 @@
-public class LongTimeNoSee {
+public class NumberChange {
     public static void main(String[] args) {
 
         int a = 1, b = 2;
---
+++ src/main/java/Racing.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Racing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 경주 게임에 오신것을 환영합니다. ID를 입력해주세요!");
+        String id = scanner.nextLine();
+
+        System.out.println("반갑습니다 " + id + "님");
+        menu();
+    }
+
+
+        public static void menu() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("");
+        }
+    }
