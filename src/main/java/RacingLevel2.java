5c43a98fb7e239eb1ab9ab7253f2b1b65dc9bd36
성정훈 <corqkrtk1@gmail.com>
2023-02-14 22:55:30 +0900

    레이싱 코드 해석

--- src/main/java/RacingLevel2.java
+++ src/main/java/RacingLevel2.java
@@ -9,7 +9,7 @@ public class RacingLevel2 {
         System.out.println("시도할 횟수를 입력하세요.");
         int tryCount = scanner.nextInt();
 
-        RacingMain racingMain = new RacingMain(carCount, tryCount);
-        racingMain.run();
+        RacingMain racingMain = new RacingMain(carCount, tryCount); //레이싱 메인 객체를 생성하여 저장값 대수, 횟수를 사용할 수 있게끔 전송? 한다.
+        racingMain.run(); //run 메소드는 실행할 동작에 대한 코드를 작성 해놓은 메소드이다.
     }
 }
--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -1,17 +1,17 @@
 import java.util.Random;
 
 public class RacingMain {
-    private int trycount;
-    private int[] carPositions;
+    private int trycount; // 정수형 변수 생성, 퍼블릭은 외부에서 값 대입이 가능하지만, 프라이베이트는 직접적으로 불가.
+    private int[] carPositions; //Private 변수에 값을 대입하기 위해서는 public 으로 제공되는 SET 함수를 사용하는 방식으로 해야합니다.<< 그냥 알아두기
 
-    public RacingMain(int carCount, int trycount) {
-        this.trycount = trycount;
-        carPositions = new int[carCount];
+    public RacingMain(int carCount, int trycount) {   //레이싱레벨2 클래스에서 저장한 변수 가지고오기,
+        this.trycount = trycount; //int carCount, int trycount를 생성자 this가 가져와서 초기화를 한다.
+        carPositions = new int[carCount]; //카포지션스 변수를 !@#$!
     }
 
     public void run() {
-        for (int i = 0; i < carPositions.length; i++) {
-            goCar();
+        for (int i = 0; i < carPositions.length; i++) { //렝스는 배열의 길이 수를 뜻함. ex) 1,2,3 = 렝스는 3
+            goCar(); //고카 메소드 실행
             System.out.println();
         }
     }
